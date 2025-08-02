22d71ad128e23986af301d08b32040dfc18651b6
Shajid <hasan.shajid222@gmail.com>
2025-08-02 13:56:25 +0600

    added new

---
+++ JavaScript/for-of.js
@@ -0,0 +1,13 @@
+// const numbers = [500, 15, 65, 75, 48 ];
+// console.log(numbers);
+
+// for(const num of numbers){
+//     console.log(num);
+// }
+
+const fruits = ['Orrange', 'Apple', 'Banana', 'Jackfruit', 'Watermelon'];
+console.log(fruits);
+for(const fruit of fruits){
+    console.log(fruit);
+    console.log('I want to eat');
+}
