c536863299515b0ba7af7e6d6b5c311dc7d2ebdf
Shajid <hasan.shajid222@gmail.com>
2025-08-03 18:08:53 +0600

    added new code

---
+++ JavaScript/Conditional Practice/ternary.js
@@ -0,0 +1,19 @@
+let num1 = 80;
+let num2 = 25;
+
+// if(num1 > num2){
+//     result = num1*2;
+//     console.log(result);
+// }
+// else{
+//     if(num1 < num2){
+//         result = num1 + num2;
+//         console.log (result);
+//     }
+// }
+
+// Ternary Operator
+
+let result = (num1 > num2) ? num1*2 : num1 + num2;
+
+console.log(result);
