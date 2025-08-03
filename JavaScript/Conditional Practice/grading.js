36ededbb75734754d9c1d61986628885754776b8
Shajid <hasan.shajid222@gmail.com>
2025-08-03 16:10:26 +0600

    added new

---
+++ JavaScript/Conditional Practice/grading.js
@@ -0,0 +1,17 @@
+let marks = 61;
+
+if(marks >=90) {
+    console.log("Your grade is : A");
+}
+else if(marks >=80) {
+    console.log("Your grade is : B");
+}
+else if(marks >=70) {
+    console.log("Your grade is : C");
+}
+else if(marks >=60) {
+    console.log("Your grade is : D");
+}
+else if(marks >=0) {
+    console.log("Your grade is : F");
+}
