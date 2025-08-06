4c24226a0f757ba254a6d6276674894c5c6ea188
Shajid <hasan.shajid222@gmail.com>
2025-08-06 21:56:36 +0600

    added new code

---
+++ JavaScript/Function/function.js
@@ -0,0 +1,27 @@
+// console.log("to day we will learn function");
+
+
+// Basic function Structure
+
+// Basic Function Example 1
+
+function hello(){
+    console.log("I am a function");
+}
+hello()
+
+// Basic Function Example 2
+
+function moneybag(){
+    var money = 4500;
+    console.log("I have "  + money + " Taka");
+}
+moneybag();
+
+// Basic Function Example 3
+function jokeMachine(input1, input2){
+    let sum = input1 + input2;
+    console.log("Sum is = " + sum);
+}
+jokeMachine(10, 40);
+jokeMachine(500, 4000);
