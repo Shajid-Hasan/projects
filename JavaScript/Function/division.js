5c389e0aedaf583bb3acb8fced7f5dedb5a241e0
Shajid <hasan.shajid222@gmail.com>
2025-08-07 01:34:24 +0600

    added new

---
+++ JavaScript/Function/division.js
@@ -0,0 +1,10 @@
+// Division Using By function
+
+function salary(january, february){
+    totalEmployee = january / february;
+    console.log("Give Govment Tax yearly = " + totalEmployee);
+    return totalEmployee;
+}
+
+const tax = salary(200, 10);
+console.log(tax);
