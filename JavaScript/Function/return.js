d92a567bc043c97ad79b29bac373cc3ff48142d1
Shajid <hasan.shajid222@gmail.com>
2025-08-09 16:43:54 +0600

    added new

---
+++ JavaScript/Function/return.js
@@ -0,0 +1,8 @@
+function tentimes (number){
+    const result = number *10;
+    console.log("The square result is : " + result);
+    return result;
+}
+
+const square = tentimes(100);
+console.log(square)
