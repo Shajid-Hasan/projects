de22ed86c1ee247f7505d655da9ac7932c078da6
Shajid <hasan.shajid222@gmail.com>
2025-08-10 23:03:12 +0600

    added new

---
+++ JavaScript/JS problems/math.js
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+
+const min = Math.min(45, 101, 56, 123, 21);
+console.log("Minimum number is : ", min);
+
+const max = Math.max(45, 101, 56, 123, 21);
+console.log("Maximum number is ", max);
+
+console.log(Math.PI);
+
+console.log(Math.abs(5-10));
+
+console.log(Math.round(2.85));
+
+console.log(Math.floor(2.99));
+console.log(Math.ceil(2.01));
+
+console.log(Math.random());
+console.log(Math.random())*10;
+
+const rand = Math.round(Math.random()*10);
+console.log(rand);
