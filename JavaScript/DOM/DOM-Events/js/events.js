6834117c9cfccbecfc56e03984c577c375f5d525
Shajid <hasan.shajid222@gmail.com>
2025-08-21 21:42:30 +0600

    added new

--- JavaScript/DOM/DOM-Events/js/events.js
+++ JavaScript/DOM/DOM-Events/js/events.js
@@ -1,1 +1,26 @@
 console.log('external file');
+
+// option 2 to handel event
+
+function makeYellow(){
+    document.body.style.backgroundColor = 'yellow';
+}
+function makeRed(){
+    document.body.style.backgroundColor = 'red';
+}
+
+// option 3: get Element by Id and then set onclick
+
+const btnMakeBlue = document.getElementById('btn-make-blue');
+// console.log(btnMakeBlue);
+btnMakeBlue.onclick = function makeBlue(){
+    document.body.style.backgroundColor = 'blue';
+}
+
+// option 3: different version
+
+const btnMakePurple = document.getElementById('btn-make-purple');
+btnMakePurple.onclick = makePurple;
+function makePurple(){
+    document.body.style.backgroundColor = 'purple';
+}
