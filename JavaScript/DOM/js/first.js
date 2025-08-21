da4f6ca53e91be686bcb86055faac3eb308b1a08
Shajid <hasan.shajid222@gmail.com>
2025-08-21 14:01:36 +0600

    added new

---
+++ JavaScript/DOM/js/first.js
@@ -0,0 +1,8 @@
+console.log('first file');
+
+const allSection = document.getElementsByTagName('section');
+console.log(allSection); 
+
+const firstTitle = document.getElementById('first-title');
+console.log(firstTitle.inerText);
+firstTitle.innerText = 'You are my first love';
