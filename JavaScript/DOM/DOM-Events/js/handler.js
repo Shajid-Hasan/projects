95528b321507664c8f8d3cbc5955cd16508b3393
Shajid <hasan.shajid222@gmail.com>
2025-08-23 01:32:47 +0600

    added new

--- JavaScript/DOM/DOM-Events/js/handler.js
+++ JavaScript/DOM/DOM-Events/js/handler.js
@@ -1,1 +1,13 @@
-console.log('event handler');
+document.getElementById('btn-update-title').addEventListener('click', function(){
+// console.log('btn clicked');
+    const pageTitleElement = document.getElementById('page-title');
+    console.log(pageTitleElement);
+    pageTitleElement.innerText = 'Updated page title text'
+})
+
+
+document.getElementById('btn-login')
+    .addEventListener('click', function(){
+    const userInfoELement = document.getElementById('user-info');
+    userInfoELement.innerText = 'user logged in succesfully';
+})
