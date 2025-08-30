8f209a05004db13f986cdafa6598a45517231581
Shajid <hasan.shajid222@gmail.com>
2025-08-31 00:45:20 +0600

    added new

---
+++ JavaScript/ES6/template-string.js
@@ -0,0 +1,28 @@
+// const poem = `Bangladesh is our mother land
+// our independent day 16th December
+// 21st february international mother language day`;
+// console.log(poem)
+
+function poem(article){
+    const script = article;
+    console.log(script)
+}
+function poem(article){
+    
+    console.log(article)
+}
+poem(`Bangladesh is our mother land
+our independent day 16th December
+21st february international mother language day`)
+
+function sum(num1, num2){
+    const result = num1 + num2
+    // const output = 'sum of' + num1 + 'and' + num2 + 'is equal to' + result;
+    const output = `sum of ${num1} and ${num2} is equal to ${result}`
+    console.log(output)
+}
+sum(20, 10)
+
+const price = 100;
+const discountPrice = `Discount price is after 20% discount ${price * 0.8} taka`
+console.log(discountPrice)
