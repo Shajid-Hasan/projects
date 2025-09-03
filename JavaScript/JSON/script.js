c221b164887021a8a516bb205e939dc94a94db5c
Shajid <hasan.shajid222@gmail.com>
2025-09-03 18:13:57 +0600

    added new

--- JavaScript/JSON/script.js
+++ JavaScript/JSON/script.js
@@ -1,40 +1,57 @@
-
 // Built in function
 
-const loadData = () =>{
-    fetch('https://jsonplaceholder.typicode.com/todos/1')
-    
+const loadData = () => {
+  fetch("https://jsonplaceholder.typicode.com/todos/1")
     // promise of response
 
-        .then(response => response.json())
+    .then((response) => response.json())
 
-    // Promise of JSON data\
-    
-        .then(json => console.log(json))
-}
+    // Promise of JSON data
 
+    .then((json) => console.log(json));
+};
 
+const loadPost = () => {
+    const url = "https://jsonplaceholder.typicode.com/posts";
 
-const person = {
-    name: 'Shajid',
-    fruit: 'apple',
-    dish: 'beef',
-    friends: ['Ratul','Robin','Rumon'],
-    isRich: false,
-    money: 20000
-}
-console.log(person, typeof person);
+  fetch(url) //promise of response
+    .then((res) => res.json()) //through to function then call res.json and promise json data
+
+    .then((json) => {
+        console.log(json);
+        displayPost(json);
+    }); //json data send to function as a parameter and call display post and send arraow of object
+};
+
+// Seen UI
+
+const displayPost = (posts) => {
+  //receive json data
+    posts.forEach((post) => {
+    console.log(post);
+});
+};
+
+// const person = {
+//     name: 'Shajid',
+//     fruit: 'apple',
+//     dish: 'beef',
+//     friends: ['Ratul','Robin','Rumon'],
+//     isRich: false,
+//     money: 20000
+// }
+// console.log(person, typeof person);
 
 /**
  * JS object with noitation
- * 
+ *
  * JSON.stringify (convert) -> JSON
- * JSON.parsr (convert) -> object 
+ * JSON.parsr (convert) -> object
  */
 // object convert to String
-const personJSON = JSON.stringify(person);
-console.log(personJSON, typeof personJSON);
+// const personJSON = JSON.stringify(person);
+// console.log(personJSON, typeof personJSON);
 
-// String convert object
-const parseJSON = JSON.parse(personJSON);
-console.log(parseJSON);
+// // String convert object
+// const parseJSON = JSON.parse(personJSON);
+// console.log(parseJSON);
