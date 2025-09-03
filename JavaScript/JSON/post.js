cb46a60f118e17ff8a3a795da3a3b994616ee6ba
Shajid <hasan.shajid222@gmail.com>
2025-09-03 19:01:21 +0600

    added new

---
+++ JavaScript/JSON/post.js
@@ -0,0 +1,31 @@
+// const url = fetch('https://jsonplaceholder.typicode.com/todos/1')
+//     .then(response => response.json())
+//     .then(json => console.log(json))
+
+const loadPost = () => {
+    const url = "https://jsonplaceholder.typicode.com/posts";
+
+    fetch(url)
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data)
+            displayPost(data)
+        });
+}
+
+// UI show
+
+const displayPost = (posts) => {
+
+    // for(let i=0; i<posts.length; i++){
+    //     console.log(posts[i]);
+    // }
+
+    // for(const post of posts){
+    //     console.log(post);
+    // }
+
+    posts.forEach((post) => {
+    console.log(post);
+    });   
+}
--- JavaScript/JSON/script.js
+++ JavaScript/JSON/script.js
@@ -1,7 +1,7 @@
 // Built in function
 
 const loadData = () => {
-  fetch("https://jsonplaceholder.typicode.com/todos/1")
+    fetch("https://jsonplaceholder.typicode.com/todos/1")
     // promise of response
 
     .then((response) => response.json())
@@ -11,26 +11,27 @@ const loadData = () => {
     .then((json) => console.log(json));
 };
 
-const loadPost = () => {
-    const url = "https://jsonplaceholder.typicode.com/posts";
+// const loadPost = () => {
+//     const url = "https://jsonplaceholder.typicode.com/posts";
 
-  fetch(url) //promise of response
-    .then((res) => res.json()) //through to function then call res.json and promise json data
+//   fetch(url) //promise of response
 
-    .then((json) => {
-        console.log(json);
-        displayPost(json);
-    }); //json data send to function as a parameter and call display post and send arraow of object
-};
+//     .then((res) => res.json()) 
 
-// Seen UI
+//     .then((json) => {
+//         console.log(json);
+//         displayPost(json);
+//     }); 
+// };
 
-const displayPost = (posts) => {
-  //receive json data
-    posts.forEach((post) => {
-    console.log(post);
-});
-};
+// // Seen UI
+
+// const displayPost = (posts) => {
+//   //receive json data
+//     posts.forEach((post) => {
+//     console.log(post);
+// });
+// };
 
 // const person = {
 //     name: 'Shajid',
