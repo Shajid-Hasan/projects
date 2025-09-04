1ce1a8abec724a586f2f813de1a083f76d30c31a
Shajid <hasan.shajid222@gmail.com>
2025-09-04 17:06:53 +0600

    added new

---
+++ JavaScript/JSON/test.js
@@ -0,0 +1,8 @@
+const data = async() => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+    console.log('Hello');
+    const json = await res.json()
+    console.log(json)
+    console.log(true);
+}
+data()
