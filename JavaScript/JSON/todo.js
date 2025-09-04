2d620889e4378c25f4ebc6e9b7a52e0245a68a8f
Shajid <hasan.shajid222@gmail.com>
2025-09-04 17:07:27 +0600

    added new

--- JavaScript/JSON/todo.js
+++ JavaScript/JSON/todo.js
@@ -18,7 +18,7 @@ const displayTodo = (todo) => {
         <div class="todo-card">
             <p>${todo.completed == true 
                 ? `<i class="fa-solid fa-square-check"></i>` 
-                : `<i class="fa-regular fa-square-check"></i>`} </p>
+                : `<i class="fa-regular fa-square-check"></i>`} </p> 
             <h4>${todo.title}</h4>
         </div>
     `
